19016a14afca84ca0cf54facfbb1ed5d9ffc5c45
murat <elbe.murat@gmail.com>
2021-05-31 22:24:42 +0200

    vue lesson 1

---
+++ frontend/vue_template_3/src/components/Child2.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="child_2">
+    Child_2 Component <br />
+    Child data: {{ deger }}
+    <br />
+    Child_1 den gelen veri: {{ child_1 }}
+    <br />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Child2",
+  props: {
+    child_1: {
+      type: [String, Number],
+      name: "mesaj",
+    },
+  },
+  data: () => {
+    return {
+      deger: "abc",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+.child_2 {
+  border: 1px solid blue;
+  height: 200px;
+}
+</style>
