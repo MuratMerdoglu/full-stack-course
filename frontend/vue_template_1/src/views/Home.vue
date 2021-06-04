2f0978e79f96291a8dc0963922eae42d5758715b
murat <elbe.murat@gmail.com>
2021-06-04 22:35:52 +0200

    templates

---
+++ frontend/vue_template_1/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <helloComponent msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    helloComponent: HelloWorld,
+  },
+};
+</script>
+
+<style></style>
